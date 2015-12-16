39d5f010108e280447d8ce2b7aaeb405d7727aff
Jonathan Galentine <j.galentine@gmail.com>
2015-12-16 15:41:55 -0500

    Setup menu

---
+++ app/src/main/java/jgalentine007/wakeonpi/MainActivity.java
@@ -0,0 +1,38 @@
+package jgalentine007.wakeonpi;
+
+import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate our menu from the resources by using the menu inflater.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.Refresh:
+                return true;
+            case R.id.About:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
