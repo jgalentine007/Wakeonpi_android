f9239e1df7201986aafe2eb7f7324f485bb60e1c
Jonathan Galentine <j.galentine@gmail.com>
2016-01-02 15:54:00 -0500

    Setup menu

--- app/src/main/java/jgalentine007/wakeonpi/MainActivity.java
+++ app/src/main/java/jgalentine007/wakeonpi/MainActivity.java
@@ -1,10 +1,12 @@
 package jgalentine007.wakeonpi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -22,12 +24,19 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
+    public void Settings(){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.Refresh:
                 return true;
+            case R.id.Settings:
+                Settings();
             case R.id.About:
                 return true;
             default:
